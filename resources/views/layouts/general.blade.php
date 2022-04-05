677515d8ea07c6589e9a9cfde651195b5462b2d3
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-05 15:17:29 +0200

    Updated bootstrap, added bootstrap navbar to header

--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -5,11 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>@yield('title')</title>
+
+        <link href="css/app.css" rel="stylesheet">
+        <script href="js/app.js"></script>
     </head>
     <body>
-        <!-- Header -->
+
         @include('components.header')
-        <!-- /Header -->
 
         <!-- Content -->
         @yield('content')
