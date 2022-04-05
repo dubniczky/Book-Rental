3d74b9ed3ce1f1e531646370177c514bb4860e70
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-05 14:43:52 +0200

    Installed bootstrap

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/welcome', function () {
 Route::get('/', function () {
     return view('index');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
