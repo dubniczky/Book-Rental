89827b5bb0a525c2d277c3b068d10d03931016e4
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-05 17:54:25 +0200

    Added user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schema::dropIfExists('users');
+        User::factory()->count(1)->create([
+            'is_librarian' => true
+        ]);
+        User::factory()->count(30)->create();
+    }
+}
