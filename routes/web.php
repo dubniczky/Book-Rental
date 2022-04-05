3ab4dd94d6cc2a66ae31a14b4b45d20271339d82
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-05 19:24:01 +0200

    Fixed book controller route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,7 +22,7 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::resource('books', 'BookController');
+Route::resource('books', BookController::class);
 
 
 
