eb2298e32fe3200cc33a654ed9378109da399183
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-05 19:23:39 +0200

    Added book listing view

---
+++ resources/views/book/list.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.general')
+
+@section('title', 'Books')
+
+@section('content')
+
+<div class="container d-flex align-items-center justify-content-center mt-5">
+    <div class="row row-cols-3">
+
+        @foreach ($books as $book)
+        <div class="card mb-3 mr-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src={{ URL::asset( $book['cover_image'] ) }} class="card-img" alt={{ URL::asset( $book['title'] ) }}>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $book['title'] }}</h5>
+                        <p class="card-text">{{ Str::substr($book['description'], 0, 200) }}...</p>
+                        <p class="card-text"><small class="text-muted">{{ $book['in_stock'] }} available for rent</small></p>
+                        <a href="/book/details/{{ $book['id'] }}" class="btn btn-primary">Details</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+
+    </div>
+</div>
+
+@endsection
