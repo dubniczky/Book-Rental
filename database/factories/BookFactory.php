f9dc39756aa283c9cdf0506248086ce1b71b2c92
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-05 16:27:53 +0200

    Added book factory items

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class BookFactory extends Factory
 {
@@ -14,7 +15,15 @@ class BookFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker->words(3, true),
+            'authors' => $this->faker->name() . ", " . $this->faker->name(),
+            'description' => $this->faker->realText(1024),
+            'released_at' => $this->faker->date(),
+            'cover_image' => 'images/sample.png',
+            'pages' => $this->faker->numberBetween(30, 800),
+            'language_code' => $this->faker->boolean() ? 'hu' : 'en',
+            'isbn' => $this->faker->uniqid(),
+            'in_stock' => $this->faker->numberBetween(1, 15),
         ];
     }
 }
