499198678dbb47c258bd964682ab14611635d9bb
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-13 00:08:40 +0200

    Added borrow factory random connection selectors

--- database/factories/BorrowFactory.php
+++ database/factories/BorrowFactory.php
@@ -15,8 +15,36 @@ class BorrowFactory extends Factory
      */
     public function definition()
     {
-        $readers = User::where('is_librarian', '=', '0')->get();
+        # Get is deprecated and it's called pluck now
 
+        # Get random reader
+        $reader = $this->faker->randomElement(
+            User::where('is_librarian', '=', '0')->pluck('id')->toArray()
+        );
+
+        # Get random request manager
+        $request_manager = $this->faker->randomElement(
+            User::where('is_librarian', '=', '1')->pluck('id')->toArray()
+        );
+
+        # Get random return manager
+        $return_manager = $this->faker->randomElement(
+            User::where('is_librarian', '=', '1')->pluck('id')->toArray()
+        );
+
+        # Get random book
+        $book = $this->faker->randomElement(
+            Book::where('in_stock', '>', '0')->pluck('id')->toArray()
+        );
+
+        # Get random state
+        $state = $this->faker->random_int(0, 3);
+
+
+        switch ($state) {
+            case 0:
+                return 
+        }
         return [
             //
         ];
