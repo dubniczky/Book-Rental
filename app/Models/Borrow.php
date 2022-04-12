6c9c61ca97a2039a7c33bcca4c2520179b36d210
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-12 23:20:46 +0200

    Added database connections to borrow model

--- app/Models/Borrow.php
+++ app/Models/Borrow.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Borrow extends Model
 {
     use HasFactory;
+
+    public function book() {
+        return $this->belongsTo(Book::class, 'book_id');
+    }
+
+    public function reader() {
+        return $this->belongsTo(User::class, 'reader_id');
+    }
+
+    public function request_manager() {
+        return $this->belongsTo(User::class, 'request_manager_id');
+    }
+
+    public function return_manager() {
+        return $this->belongsTo(User::class, 'return_manager_id');
+    }
 }
