f54e740cf79a0c4d3973ee7a7622c517c15a1ccd
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-12 23:30:27 +0200

    Cleaned up book factory

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -3,14 +3,11 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 class BookFactory extends Factory
 {
     /**
      * Define the model's default state.
-     *
-     * @return array
      */
     public function definition()
     {
@@ -20,9 +17,9 @@ class BookFactory extends Factory
             'description' => $this->faker->realText(1024),
             'released_at' => $this->faker->date(),
             'cover_image' => 'images/sample.png',
-            'pages' => $this->faker->numberBetween(30, 800),
+            'pages' => $this->faker->numberBetween(30, 500),
             'language_code' => $this->faker->boolean() ? 'hu' : 'en',
-            'isbn' => $this->faker->isbn13(),
+            'isbn' => $this->faker->isbn13(), # Apparently it has a built-in method
             'in_stock' => $this->faker->numberBetween(1, 15),
         ];
     }
