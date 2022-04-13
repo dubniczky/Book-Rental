6836c4e0d51de67c0feefcc46a551b1644fa9c51
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-13 11:17:03 +0200

    Added fixed demo users to user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,9 +15,23 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        # Create demo librarian
         User::factory()->count(1)->create([
-            'is_librarian' => true
+            'is_librarian' => true,
+            'name' => 'Demo Librarian',
+            'email' => 'librarian@brs.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
+
+        # Create demo reader
+        User::factory()->count(1)->create([
+            'is_librarian' => false,
+            'name' => 'Demo Reader',
+            'email' => 'reader@brs.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
+
+        # Create random users
         User::factory()->count(30)->create();
     }
 }
