46b6e199b02ae9b05c340e32d2beb2e0918ae0ec
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-13 11:26:56 +0200

    Added genre-book seeder

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Genre;
+use App\Models\Book;
 
 class GenreSeeder extends Seeder
 {
@@ -15,5 +16,14 @@ class GenreSeeder extends Seeder
     public function run()
     {
         Genre::factory()->count(15)->create();
+
+        $genres = Genre::all();
+        $gen_count = count($genres);
+        Book::all()->each(function($book) {
+            $count = rand(1, 5);
+            $book->genres()->attach(
+                $genres->random( $count )->pluck('id')->toArray()
+            );
+        });
     }
 }
