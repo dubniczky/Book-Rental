1566a0117e5ef4ab7d11c1d27bb4d920c710c3d5
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-16 16:22:06 +0200

    Added old form data autofill

--- resources/views/book/formarea.blade.php
+++ resources/views/book/formarea.blade.php
@@ -1,3 +1,4 @@
 <label for="{{ $name }}" class="form-label">{{ $title }}</label>
 <textarea name="{{ $name }}" class="form-control @error($name) is-invalid @enderror">
+{{ old($name) }}
 </textarea>
--- resources/views/book/formfield.blade.php
+++ resources/views/book/formfield.blade.php
@@ -1,2 +1,12 @@
 <label for="{{ $name }}" class="form-label">{{ $title }}</label>
-<input type="{{ $type }}" name="{{ $name }}" class="form-control @error($name) is-invalid @enderror" required>
+<input type="{{ $type }}"
+       name="{{ $name }}"
+       class="form-control @error($name) is-invalid @enderror"
+       required
+       value="{{ old($name) }}">
+
+@error($name)
+<div class="invalid-feedback">
+    {{$message}}
+</div>
+@enderror
